8f0dd0ad46f8ef4cb7b282fdb8d42b4da26da4c8
sogoodnow <sogoodnow@sina.com>
2018-03-15 23:42:06 +0800

    Initial commit

---
+++ aa.py
@@ -0,0 +1,8 @@
+# asdfasdfasdf
+age = input("what is your age?\n")
+print("your age is:", age)
+name = input("what is your name?\n")
+print("hi,", name, name, name)
+blance = input("what is your blance?\n")
+print("your blacne is ", blance)
+print("my name is ", name, "my age is ", age, "my blance is ", blance)
