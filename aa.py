5ad604738d988a07d8f3e23dbeca1a6b1301761e
sogoodnow <sogoodnow@sina.com>
2018-03-15 23:47:54 +0800

    del

--- aa.py
+++
@@ -1,8 +0,0 @@
-# asdfasdfasdf
-age = input("what is your age?\n")
-print("your age is:", age)
-name = input("what is your name?\n")
-print("hi,", name, name, name)
-blance = input("what is your blance?\n")
-print("your blacne is ", blance)
-print("my name is ", name, "my age is ", age, "my blance is ", blance)
