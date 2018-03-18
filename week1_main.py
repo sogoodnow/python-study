ba028e36b7d2909b4e6c9ecdad09ab32d0cb6d6a
sogoodnow <sogoodnow@sina.com>
2018-03-18 21:52:13 +0800

    change name and split moduel

---
+++ week1_main.py
@@ -0,0 +1,53 @@
+"""
+Python学习第一周作业
+本周共计3个作业：
+1) 使用while和for…in两个循环分别输出四种九九乘法表效果（共计8个）。
+2) 使用文件和目录操作，定义一个统计指定目录大小的函数（注意目录中还有子目录）。
+3) 参考1.10的综合案例中的学生信息管理，来实现一个自动取款机的存取款模拟效果。
+要求有登陆和退出、查询余额、取钱，存钱等操作。
+"""
+import week1_multi99  # 99乘法表
+import week1_file  # 文件计算
+
+
+def work3():
+    print("ccc")
+
+
+def common_print():
+    """用于主函数执行后输出的共通信息
+    return ：返回值为接收到的终端录入命令
+    """
+    print("完成 ！")
+    print("输入：1 九九乘法表  输入：2 目录大小统计  输入 ：3  学生信息管理")
+
+
+if __name__ == '__main__':
+    print(r"Python学习第一周作业本周共计3个作业："+"\n"
+          r"1) 使用while和for…in两个循环分别输出四种九九乘法表效果（共计8个）。"+"\n"
+          r"2) 使用文件和目录操作，定义一个统计指定目录大小的函数（注意目录中还有子目录）。"+"\n"
+          r"3) 参考1.10的综合案例中的学生信息管理，来实现一个自动取款机的存取款模拟效果。"+"\n"
+          r"要求有登陆和退出、查询余额、取钱，存钱等操作。"+"\n"
+          r"学员 ：邱国昌 日期：2018-03"+"\n")
+    print("※"*15)
+    while True:
+        workNo = 0
+        workNo = int(input("请输入作业编号："))
+        if workNo == 1:
+            if week1_multi99.work1():  # 调99乘法表
+                common_print()
+        elif workNo == 2:
+            path = input("请输入文件目录：")
+            count_all = 0
+            count_all = week1_file.count_size(path)
+            if count_all:  # 调文件大小统计
+                print("合计大小为：", count_all)
+                common_print()
+        elif workNo == 3:
+            if work3():  # 调学员管理系统
+                common_print()
+        else:
+            print("无效输入！")
+            break
+
+
