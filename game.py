989f5e80696ef3217858fe0f35bc93c1617dec86
sogoodnow@sina.com <qiuguochang2017>
2018-03-23 16:46:30 +0800

    添加game文件夹等

--- game.py
+++ game.py
@@ -4,6 +4,10 @@ import time
 
 
 def key_control():
+    """
+    用于获取键盘控制操作,并进行对应处理
+    :return:
+    """
     for ev in pygame.event.get():
         if ev.type == QUIT:
             exit()
@@ -19,7 +23,7 @@ def main():
     backround = pygame.image.load("./1.jpg")
     m = -968
     while True:
-        # 绘制画面
+        # 绘制画面,blit:draw one image onto another 理解为将源图片刷入某一区域或另一图片
         screen.blit(backround, (0, m))
         key_control()
         m += 2
