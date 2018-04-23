489e53df460dd3374aa80600561dab8173750444
sogoodnow <sogoodnow@sina.com>
2018-04-23 23:16:37 +0800

    week53

--- week5/myadmin/urls.py
+++ week5/myadmin/urls.py
@@ -4,11 +4,13 @@ from .views import index,users
 urlpatterns = [
     # 后台首页
     url(r'^$',index.index ,name="myadmin_index"),
-    url(r'^users$',users.index ,name="myadmin_users_index"),
+    url(r'^users/(?P<pIndex>[0-9]+)$',users.index ,name="myadmin_users_index"),
     url(r'^users/add$',users.add ,name="myadmin_users_add"),
     url(r'^users/insert$',users.insert ,name="myadmin_users_insert"),
     url(r'^users/del/(?P<uid>[0-9]+)$',users.delete ,name="myadmin_users_del"),
     url(r'^users/edit/(?P<uid>[0-9]+)$',users.edit ,name="myadmin_users_edit"),
     url(r'^users/update/(?P<uid>[0-9]+)$',users.update ,name="myadmin_users_update"),
+    url(r'^users/restpwd/(?P<uid>[0-9]+)$',users.restpwd ,name="myadmin_users_restpwd"),
+    url(r'^users/pwdupdate/(?P<uid>[0-9]+)$',users.pwdupdate ,name="myadmin_users_pwdupdate"),
 
 ]
--- week5/myadmin/views/users.py
+++ week5/myadmin/views/users.py
@@ -2,11 +2,19 @@ from django.shortcuts import render,redirect,reverse
 from django.http import HttpResponse
 from common.models import Users
 from datetime import datetime
+from django.core.paginator import Paginator
 
-def index(request):
+def index(request,pIndex = 1):
+    print(pIndex)
     ulist = Users.objects.all()
-    context = {"userslist":ulist}
-    return render(request,"myadmin/users/index.html",context)
+    p = Paginator(ulist, 3)
+    if pIndex == '':
+        pIndex = '1'
+    pIndex = int(pIndex)
+    plist = p.page(pIndex)
+    pnums = p.page_range
+    context = {'plist': plist, 'pnums': pnums, 'pIndex': pIndex}
+    return render(request, "myadmin/users/index.html", context)
 
 def add(request):
     return render(request, "myadmin/users/add.html")
@@ -37,9 +45,33 @@ def insert(request):
 
 
 def edit(request,uid):
-    pass
+    try:
+        ob = Users.objects.get(id = uid)
+        context = {"user": ob}
+        return render(request, "myadmin/users/edit.html", context)
+    except Exception as e:
+        print(e)
+        context = {"info":"修改出错！"}
+        return render(request,"myadmin/info.html",context)
+
+
 def update(request,uid):
-    pass
+    try:
+        ob = Users.objects.get(id=uid)
+        ob.name = request.POST['name']
+        ob.sex = request.POST['sex']
+        ob.address = request.POST['address']
+        ob.code = request.POST['code']
+        ob.phone = request.POST['phone']
+        ob.email = request.POST['email']
+        ob.state = request.POST['state']
+        ob.save()
+        context = {"info": "编辑成功！"}
+    except Exception as e:
+        print(e)
+        context = {"err":"编辑失败！"}
+    return render(request,"myadmin/info.html",context)
+
 def delete(request,uid):
     try:
         Users.objects.get(id = uid).delete()
@@ -47,4 +79,24 @@ def delete(request,uid):
     except Exception as e:
         print(e)
         context = {"info":"删除出错！"}
+    return render(request,"myadmin/info.html",context)
+
+def restpwd(request,uid):
+    ob = Users.objects.get(id = uid)
+    context = {"user":ob}
+    return render(request,"myadmin/users/pwdedit.html",context)
+
+def pwdupdate(request,uid):
+    try:
+        ob = Users.objects.get(id=uid)
+        newpwd = request.POST['password1']
+        import hashlib
+        m = hashlib.md5()
+        m.update(bytes(newpwd,encoding="utf8"))
+        ob.password = m.hexdigest()
+        ob.save()
+        context = {"info": "更改成功！"}
+    except Exception as e:
+        print(e)
+        context = {"info": "更改失败！"}
     return render(request,"myadmin/info.html",context)
