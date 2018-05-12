44f9a30daff6e2d2e6c8102a4c744efe2a896837
sogoodnow <sogoodnow@sina.com>
2018-05-12 15:44:56 +0800

    week7-5

--- week7/homework4.py
+++ week7/homework4.py
@@ -2,8 +2,8 @@ import requests,re,lxml
 from bs4 import BeautifulSoup
 
 s = requests.session()
+res_list=[]
 def get_data(pidnex):
-
     url = 'http://maoyan.com/board/4?offset='+str(pidnex)
     headers = {
         'User-Agent': 'User-Agent:Mozilla/5.0(WindowsNT6.1;rv:2.0.1)Gecko/20100101Firefox/4.0.1'
@@ -11,7 +11,45 @@ def get_data(pidnex):
     try:
         res = s.get(url,headers=headers).content.decode('utf-8')
         soup = BeautifulSoup(res,'lxml')
-        print(soup.select(".board-wrapper dd "))
+        # 找到对应列表
+        dilist = soup.select(".board-wrapper dd ")
+        # 遍历信息
+        for i in dilist:
+            # 存放字典
+            dic = {}
+
+            # 文字类信息
+            mes = i.text.strip('\n').split()
+            if mes[0]:
+                dic["排名"] = mes[0]
+            else:
+                dic["排名"] = ""
+            if mes[1]:
+                dic["片名"] = mes[1]
+            else:
+                dic["片名"] = ""
+            if mes[2]:
+                dic["主演"] = mes[2]
+            else:
+                dic["主演"] = ""
+            if mes[3]:
+                dic["上映时间"] = mes[3]
+            else:
+                dic["上映时间"] = ""
+            if mes[4]:
+                dic["得分"] = mes[4]
+            else:
+                dic["得分"] = ""
+            res_list.append(dic)
+            # 图片路径
+            if i.select(".board-img"):
+                dic["图片"] = i.select(".board-img")[0].get('data-src')
+            else:
+                dic["图片"] = ""
+        if int(pidnex) == 90:
+            return res_list
+        else:
+           get_data(int(pidnex)+10)
     except Exception as e:
         if hasattr(e,'code'):
             print("请求访问出错")
@@ -21,3 +59,8 @@ def get_data(pidnex):
 
 if __name__ == '__main__':
     get_data(0)
+    # for item in res_list:
+    #     print(item)
+    with open("./movie.txt",'w',encoding='utf-8') as f:
+        for line in res_list:
+            f.writelines(str(line)+'\n')
--- week7/test6.py
+++ week7/test6.py
@@ -3,21 +3,40 @@ from bs4 import BeautifulSoup
 with open('./test.html','r',encoding='UTF-8') as f:
     data = f.read()
 
-    soup = BeautifulSoup(data,"lxml")
-    dlist = soup.select('.listUl li')
-    for tag in dlist:
-        # 标题
-        title = tag.select(".des h2 a")[0].get_text()
-        print(title)
-        # 图片
-        pic = tag.select(".img_list a img[lazy_src]")[0].get('lazy_src')
-        print(pic)
-        # 户型
-        roomtype = tag.select(".des p")[0].get_text()
-        roomtype = ''.join(re.split(r'\s+',str(roomtype)))
-        print(roomtype)
-        # 价格
-        money = tag.select(".money b")[0].get_text()+"元"
-        print(money)
-        dic = {'title':title,'pic':pic,'roomtype':roomtype,'money':money}
+    # soup = BeautifulSoup(data,"lxml")
+    # dlist = soup.select('.listUl li')
+    # for tag in dlist:
+    #     # 标题
+    #     title = tag.select(".des h2 a")[0].get_text()
+    #     print(title)
+    #     # 图片
+    #     pic = tag.select(".img_list a img[lazy_src]")[0].get('lazy_src')
+    #     print(pic)
+    #     # 户型
+    #     roomtype = tag.select(".des p")[0].get_text()
+    #     roomtype = ''.join(re.split(r'\s+',str(roomtype)))
+    #     print(roomtype)
+    #     # 价格
+    #     money = tag.select(".money b")[0].get_text()+"元"
+    #     print(money)
+    #     dic = {'title':title,'pic':pic,'roomtype':roomtype,'money':money}
+    # res = s.get(url, headers=headers).content.decode('utf-8')
+    soup = BeautifulSoup(data, 'lxml')
+    dilist = soup.select(".board-wrapper dd ")
+    for i in dilist:
+        print(i.find('i').string)
+        print(i.select(".board-img")[0].get('alt'))
+        print(i.select(".board-img")[0].get('data-src'))
+
+        # print(i.text.split('\s+?\w\s+?'))
+        # pat = re.compile('\s+?(\w+?)\s+?(\w+?)')
+        # print(re.findall(pat,i.text))
+        # print(re.findall('\S*',i.text))
+        print(i.text.strip('\n').split())
+    # 排名
+    # 片名
+    # 图片
+    # 主演
+    # 上映时间
+    # 得分
 
