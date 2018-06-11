c1e010a8852ee032a37b588e104f7a0eaef9f9cf
sogoodnow@sina.com <qiuguochang2017>
2018-06-11 16:57:25 +0800

    week10-5

--- week10/jd/jd/pipelines.py
+++ week10/jd/jd/pipelines.py
@@ -6,26 +6,26 @@ import pymongo
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 class MongoPileline(object):
-    def __init__(self,uri,db):
-        self.uri = uri
-        self.db = db
-        self.client = None
-        self.collection = None
+    def __init__(self,MONGO_URI,MONGO_DB):
+        self.MONGO_URI = MONGO_URI
+        self.MONGO_DB = MONGO_DB
 
     @classmethod
     def from_crawler(cls, crawler):
         return cls(
-            uri=crawler.settings.get("MONGO_URI"),
-            db=crawler.settings.get("MONGO_DB")
+            MONGO_URI = crawler.settings.get("MONGO_URI"),
+            MONGO_DB = crawler.settings.get("MONGO_DB")
         )
 
     def open_spider(self,spider):
-        self.client = pymongo.MongoClient(self.uri)
-        self.collection = self.client[self.db]
+        self.client = pymongo.MongoClient(self.MONGO_URI)
+        self.db = self.client[self.MONGO_DB]
 
 
     def process_item(self, item, spider):
-        pass
+        collection_name = item.__class__.__name__
+        self.db[collection_name].insert(dict(item))
+        return item
 
 
     def close_spider(self,spider):
