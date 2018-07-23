b545c62852093b63c346a8631be0dcd7f9cea2c4
sogoodnow@sina.com <qiuguochang2017>
2018-07-23 11:05:45 +0800

    week13-7

---
+++ week13/backend/demo1.py
@@ -0,0 +1,14 @@
+# pip install flask pymysql flask-sqlalchemy flask-sqlacodegen
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+if __name__ =='__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
