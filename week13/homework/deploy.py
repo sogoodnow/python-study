d8013baae2fd2000f37be1b57caef027c830f4b5
sogoodnow@sina.com <qiuguochang2017>
2018-07-26 17:11:20 +0800

    week13-monitor finish

--- week13/homework/deploy.py
+++ week13/homework/deploy.py
@@ -2,3 +2,6 @@ import paramiko
 
 trassport = paramiko.Transport("192.168.204.128",22)
 trassport.connect(username="root",password="root")
+sftp = paramiko.SFTPClient.from_transport(trassport)
+
+sftp.put()
