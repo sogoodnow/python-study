7018c0138acd1dd68f0ba32cdde1f771d01e6265
sogoodnow <sogoodnow@sina.com>
2018-09-06 22:19:31 +0800

    w16-first ok

---
+++ week16/ML/tree.py
@@ -0,0 +1,11 @@
+import numpy as py
+from collections import Counter
+
+class DecisionTreeClassfiy():
+
+    def fit(self,X,y):
+        
+        return self
+
+    def predict(self,X):
+        pass
